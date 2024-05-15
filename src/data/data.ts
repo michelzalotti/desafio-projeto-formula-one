5800bb90f859fdac1e165ae289b84b478a764cda
Michel Zalotti <michelzalotti@gmail.com>
2024-05-14 21:10:51 -0300

    Separando os dados

---
+++ src/data/data.ts
@@ -0,0 +1,20 @@
+export const teams = [
+  { id: 1, name: "Ferrari" },
+  { id: 2, name: "McLaren" },
+  { id: 3, name: "Red Bull Racing" },
+  { id: 4, name: "Aston Martin Aramco Formula One Team" },
+  { id: 5, name: "BWT Alpine F1 Team" },
+  { id: 6, name: "Mercedes-AMG Petronas Formula One Team" },
+  { id: 7, name: "MoneyGram Haas F1 Team" },
+  { id: 8, name: "Oracle Red Bull Racing" },
+  { id: 9, name: "Scuderia Ferrari" },
+  { id: 10, name: "Stake F1 Team Kick Sauber" },
+  { id: 11, name: "Visa Cash App RB Formula One Team" },
+  { id: 12, name: "Williams Racing" },
+];
+
+export const drivers = [
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
+  { id: 3, name: "Lando Norris", team: "McLaren" },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
+import cors from "@fastify/cors";
 import "./server";
 import { app } from "./server";
-import cors from "@fastify/cors";
-
-const teams = [
-  { id: 1, name: "Ferrari" },
-  { id: 2, name: "McLaren" },
-  { id: 3, name: "Red Bull Racing" },
-  { id: 4, name: "Aston Martin Aramco Formula One Team" },
-  { id: 5, name: "BWT Alpine F1 Team" },
-  { id: 6, name: "Mercedes-AMG Petronas Formula One Team" },
-  { id: 7, name: "MoneyGram Haas F1 Team" },
-  { id: 8, name: "Oracle Red Bull Racing" },
-  { id: 9, name: "Scuderia Ferrari" },
-  { id: 10, name: "Stake F1 Team Kick Sauber" },
-  { id: 11, name: "Visa Cash App RB Formula One Team" },
-  { id: 12, name: "Williams Racing" },
-];
-
-const drivers = [
-  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
-  { id: 3, name: "Lando Norris", team: "McLaren" },
-];
+import { teams, drivers } from "./data/data";
 
 app.register(cors, {
   origin: "*",
